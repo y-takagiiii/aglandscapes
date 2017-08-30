6efbfff0c68f93095164714dc6210d010ba715f9
DESKTOP-3H64MLL\okaya <yy780701@yahoo.co.jp>
2017-08-30 11:43:54 +0900

    トップページの最終版です。

--- aglandscapes/top.php
+++ aglandscapes/top.php
@@ -32,27 +32,27 @@ if (!empty($_POST)){
       //存在してなかったら、ログイン失敗のマークを保存
       $error['login'] = 'failed';
     
+    }else{
+      //ログイン成功
+      //ログインしている人のmember_idをSESSIONに保存
+      //現在の時刻を表す情報をSESSIONに保存（1時間を使用していない場合に自動でログアウトさせるため）
+      $_SESSION['login_member_id']=$record['member_id'];
+      //どの画面でも保存されているデータ
+
+      $_SESSION['time']=time();
+      //1970年1月1日０時0分０秒から現在までの秒数が保存される
+
+      header('Location:top.php#search');
+      exit();
+    }
+
   }else{
-//ログイン成功
-  //ログインしている人のmember_idをSESSIONに保存
-  //現在の時刻を表す情報をSESSIONに保存（1時間を使用していない場合に自動でログアウトさせるため）
-    $_SESSION['login_member_id']=$record['member_id'];
-    //どの画面でも保存されているデータ
-
-    $_SESSION['time']=time();
-    //1970年1月1日０時0分０秒から現在までの秒数が保存される
-
-    header('Location:top.php#search');
-    exit();
-  }
-
- }else{
    
     //存在していたら、index.phpへ移動、存在していなかったら、ログイン失敗のマークを保存
     $error ['login'] = 'blank';
 
   }
-  }
+}
     // articlesより全てのデータを取ってくる
 
 $page = '';
@@ -74,21 +74,18 @@ $page =max($page,1);
 //データの件数から最大ページ数を計算する
 
 
-
-//宿題：このSQL文を実行して、取得したデータ数をVar_dumpで表示しましょう。
-
-$sql = "SELECT COUNT(*) AS `cnt` FROM `articles` WHERE `delete_flag`=0";
+$sql = "SELECT COUNT(*) AS `cnt` FROM `articles` ";
 
   $stmt=$dbh->prepare($sql);
   $stmt->execute();
   $cnt=$stmt->fetch(PDO::FETCH_ASSOC);
-  var_dump($cnt['cnt']);
+  // var_dump($cnt['cnt']);
 
 
 
 $start = 0;
 
-$card_number = 6; //1 ページに何個つぶやきをだすか指定
+$card_number = 6; //1 ページに何個artcileをだすか指定
 
 $max_page = ceil($cnt['cnt'] / $card_number);
 
@@ -99,16 +96,40 @@ $page = min($page,$max_page);
 
 $start =($page -1) * $card_number;
 
-$sql = sprintf('SELECT * FROM `articles`  ORDER BY `articles`.`created`
-       DESC LIMIT  %d,%d',$start,$tweet_number);
+$sql = sprintf('SELECT * FROM `products` INNER JOIN (`articles` INNER JOIN `prefectures` ON `articles`.`prefecture_id`=`prefectures`.`prefecture_id`) ON `products`.`product_id`=`articles`.`product_id` ORDER BY `articles`.`created` DESC LIMIT  %d,%d',$start,$card_number);
 
+    // articles&prefectures&productsより全てのデータを取ってくる
+   
 //SQL文実行
 $stmt = $dbh->prepare($sql);
 $stmt->execute();
-
-$articles=array();
+$article=array();
+// $articles=array();
 //データを取得して配列に保存
 while ($record = $stmt->fetch(PDO::FETCH_ASSOC)){
+
+      if(isset( $_SESSION['login_member_id']) && ($_SESSION['login_member_id']==$record['member_id'])){
+
+         // favorite状態の取得（ログインユーザーごと）
+    $sql='SELECT COUNT(*) as `favorite_count` FROM `favorites` WHERE `article_id`='.$record['article_id'].' AND `member_id`='.$_SESSION['login_member_id'];
+ 
+    // sql文実行
+    $stmt_flag=$dbh->prepare($sql);
+    $stmt_flag->execute();
+    $favorite_cnt=$stmt_flag->fetch(PDO::FETCH_ASSOC);
+
+        //apply状態の取得
+    $sql='SELECT COUNT(*) as `apply_count` FROM `applies` WHERE `article_id`='.$record['article_id'].' AND `member_id`='.$_SESSION['login_member_id'];
+    // sql文実行
+    $stmt_flag=$dbh->prepare($sql);
+    $stmt_flag->execute();
+    $apply_cnt=$stmt_flag->fetch(PDO::FETCH_ASSOC);
+  }else{
+
+    //ログインしてない場合
+    $favorite_cnt['favorite_count']=0;
+    $apply_cnt['apply_count']=0;
+  }
 
     // 全件配列に入れる
     $article[]=array(
@@ -132,10 +153,66 @@ while ($record = $stmt->fetch(PDO::FETCH_ASSOC)){
     "treatment6"=>$record['treatment6'],
     "landscapes"=>$record['landscapes'],
     "comment"=>$record['comment'],
-    "favorite_flag"=>$favorite_cnt
+    "favorite_flag"=>$favorite_cnt['favorite_count'],
+    "apply_flag"=>$apply_cnt['apply_count']
     );
   }
 
+  // おすすめ表示
+  // sql文(favorites,articlesをテーブル結合)
+$sql =sprintf('SELECT * FROM `articles` INNER JOIN `favorites` ON `articles`.`article_id`=`favorites`.`article_id` WHERE `favorites`.`member_id`= 5 ORDER BY `articles`.`created` DESC limit %d,%d',$start,$card_number);
+
+  // sql文の実行
+$stmt = $dbh->prepare($sql);
+$stmt->execute();
+
+//   データを取得して配列に保存
+while ($record1 = $stmt->fetch(PDO::FETCH_ASSOC)){
+
+// prefecturesテーブルから都道府県名を取得して代入(selsect文)
+$sql = 'SELECT `prefecture` FROM `prefectures` WHERE `prefecture_id`='.$record1['prefecture_id'].' ';
+// SQL文実行
+$stmt2 = $dbh->prepare($sql);
+$stmt2->execute();
+$record2 = $stmt2->fetch(PDO::FETCH_ASSOC);
+
+
+// productsテーブルから作物名を取得して代入(select文)
+$sql = 'SELECT `product` FROM `products` WHERE `product_id`='.$record1['product_id'].' ';
+// sql文の実行
+$stmt3 = $dbh->prepare($sql);
+$stmt3->execute();
+$record3 = $stmt3->fetch(PDO::FETCH_ASSOC);
+
+
+
+    // 全件配列に入れる
+    $admin_article[]=array(
+    "article_id"=>$record1['article_id'],
+    "member_id"=>$record1['member_id'],
+    "title"=>$record1['title'],
+    "prefecture_id"=>$record1['prefecture_id'],
+    "prefecture"=>$record2['prefecture'],
+    "place"=>$record1['place'],
+    "access"=>$record1['access'],
+    "start"=>$record1['start'],
+    "finish"=>$record1['finish'],
+    "product_id"=>$record1['product_id'],
+    "product"=>$record3['product'],
+    "work"=>$record1['work'],
+    "treatment1"=>$record1['treatment1'],
+    "treatment2"=>$record1['treatment2'],
+    "treatment3"=>$record1['treatment3'],
+    "treatment4"=>$record1['treatment4'],
+    "treatment5"=>$record1['treatment5'],
+    "treatment6"=>$record1['treatment6'],
+    "landscapes"=>$record1['landscapes'],
+    "comment"=>$record1['comment'],
+     "favorite_flag"=>$favorite_cnt['favorite_count'],
+    "apply_flag"=>$apply_cnt['apply_count']
+    // "favorite_flag"=>$favorite_cnt
+    );
+  }
 
 ?>
 
@@ -161,8 +238,10 @@ while ($record = $stmt->fetch(PDO::FETCH_ASSOC)){
     <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet">
     <link href="assets/css/form.css" rel="stylesheet">
     <link href="assets/css/top_timeline.css" rel="stylesheet">
-    <link href="assets/css/top_main.css" rel="stylesheet">
-    <link href="assets/css/top_ag_original.css" rel="stylesheet">
+    <link href="assets/css/anly_main.css" rel="stylesheet">
+    <link href="assets/css/anly_ag_original.css" rel="stylesheet">
+    <link href="assets/css/body.css" rel="stylesheet">
+
     <!--
       designフォルダ内では2つパスの位置を戻ってからcssにアクセスしていることに注意！
      -->
@@ -184,8 +263,8 @@ while ($record = $stmt->fetch(PDO::FETCH_ASSOC)){
                <div class="container">
                   <div class="hero-text align-center">
                      <h1>AglandScapes</h1>
-                      <p>農業に新たな活力を！　旅行に新たな感動を!</p>
-                       <p>そして、その出会いは人生の新たな一歩へ!</p>
+                      <h2 style="color: #FFFFFF">農業に新たな活力を！　旅行に新たな感動を!</h2>
+                       <h2 style="color: #FFFFFF">そして、その出会いは人生の新たな一歩へ!</h2>
                   </div>
 
                     <!--Login Section-->
@@ -193,7 +272,7 @@ while ($record = $stmt->fetch(PDO::FETCH_ASSOC)){
                         
                         <form method="POST" action="" class="form-signin mg-btm" name="login">
                            <div class="social-box">
-                              <div class="row mg-btm">
+<!--                               <div class="row mg-btm">
                                   <div class="col-md-12">
                                      <a href="#" class="btn btn-primary">
                                        <i class="icon-facebook"></i>  Facebookアカウントでログイン
@@ -206,7 +285,7 @@ while ($record = $stmt->fetch(PDO::FETCH_ASSOC)){
                                        <i class="icon-twitter"></i>   Twitterアカウントでログイン
                                      </a>
                                  </div>
-                               </div>
+                               </div> -->
                                   <?php if(isset($error['login']) && ($error['login']=='blank')) {?>
               <!--issetこの変数は存在していた時-->
               <h4 class="error">*メールアドレスとパスワードをご記入ください。</h4>
@@ -220,7 +299,7 @@ while ($record = $stmt->fetch(PDO::FETCH_ASSOC)){
               <?php }?>
                             <div class="main">   
         
-                               <input name="email" type="text" class="form-control" placeholder="メールアドレス" autofocus>
+                               <input name="email" type="text" class="form-control" placeholder="メールアドレス">
                                <input name="password" type="password" class="form-control" placeholder="パスワード">
                               <a href="javascript:void(0)" onclick="document.login.submit();return false;" type="submit" class="btn btn-large btn-danger pull-right">ログイン</a>
                            　</div>
@@ -233,32 +312,37 @@ while ($record = $stmt->fetch(PDO::FETCH_ASSOC)){
 
                          <section class="">
                          <div class="section section-hero" style="color: #FFFFFF">
-                        
-                             <h3>AGLANDSCAPESとは </h3>
-                              <h3 class="text-left">農家さんと農業に関心がある、学びたいと思っている方（体験者）をつなぐ役割</h3>
-                              <h3 class="text-left">を担うのがAglandscapes。Agri(農業）＋landscapes(風景）で、農業のある風景</h3>
-                              <h3 class="text-left">を将来に残したい。そんな思いから出来上がったサービスです。   </h3>
+                              <h3 class="text-center">農家さんと農業に関心がある、学びたいと思っている方（体験者）をつなぐ役割</h3>
+                              <h3 class="text-center">を担うのがAglandscapes。Agri(農業）＋landscapes(風景）で、農業のある風景</h3>
+                              <h3 class="text-center">を将来に残したい。そんな思いから出来上がったサービスです。　　　　　　　</h3>
                               
                          </div>
+
+                         <div name="search" id="search">
+                           
+                           
+                         </div> 
                         </div>
                     </div>       
               </div>
 
             <!-- Statistics Box -->
 
-            
-            <div class="container">
+         
+                    
+            <div   class="container">
                <div class="statistics-box">
                   <div class="statistics-item">
-                     <a name="search" href="genre_search.php" class="value">都道府県</a>
+
+                     <a name="" href="genre_search.php?type=pre"  class="value">都道府県</a>
                   </div>
 
                   <div class="statistics-item">
-                     <a name="search" href="genre_search.php" class="value">期間</a>
+                     <a name="" href="genre_search.php?type=term"  class="value">期間</a>
                   </div>
 
                  <div class="statistics-item">
-                     <a name="search" href="genre_search.php" class="value">作物</a>
+                     <a name="" href="genre_search.php?type=product"  class="value">作物</a>
                   </div>
                   
                </div>
@@ -272,74 +356,74 @@ while ($record = $stmt->fetch(PDO::FETCH_ASSOC)){
             <!-- Title -->
             <div class="section-title">
                <div class="container">
-                  <h2 class="title">新着募集</h2>
+
+                  <h2 id="new_article" name="new_article" class="title">新着募集</h2>
                </div>
+            </div>
 
 
             <!-- card section-->
-        
-         <div class="container">
-          <div class="row">
-            <hr>
-            <div class="row row-margin-bottom">
+ <div class="container"> 
+ <div class="row">
+            
+           
+             <!-- <div class="row row-margin-bottom"> -->
       <!-- card section-->
-             <?php foreach ($article as $article_each) { ?>
+    <?php if (isset($article)) { ?>
+    <?php foreach ($article as $article_each) { ?>
+ <!-- <div class="col-md-6 col-sm-6 col-xs-12">       -->
+     <?php $article_id=$article_each['article_id']; ?>
+    <?php $title=$article_each['title']; ?>
+    <?php $prefecture=$article_each['prefecture']; ?>
+    <?php $place=$article_each['place']; ?>
+    <?php $access=$article_each['access']; ?>
+    <?php $start=$article_each['start']; ?>
+    <?php $finish=$article_each['finish']; ?>
+    <?php $product=$article_each['product']; ?>
+    <?php $work=$article_each['work']; ?>
+    <?php $treatment1=$article_each['treatment1']; ?>
+    <?php $treatment2=$article_each['treatment2']; ?>
+    <?php $treatment3=$article_each['treatment3']; ?>
+    <?php $treatment4=$article_each['treatment4']; ?>
+    <?php $treatment5=$article_each['treatment5']; ?>
+    <?php $treatment6=$article_each['treatment6']; ?>
+    <?php $landscape=$article_each['landscapes']; ?>
+    <?php $comment=$article_each['comment']; ?>
+    <?php if(isset($article_each['favorite_flag'])){$favorite_flag=$article_each['favorite_flag'];} ?> 
+    <?php if(isset($article_each['apply_flag'])){$apply_id=$article_each['apply_flag'];} ?>
 
-              <?php $article_id=$article_each['article_id']; ?>
-               <?php $title=$article_each['title']; ?>
-               <?php $prefecture=$article_each['prefecture']; ?>
-               <?php $place=$article_each['place']; ?>
-               <?php $access=$article_each['access']; ?>
-               <?php $start=$article_each['start']; ?>
-               <?php $finish=$article_each['finish']; ?>
-               <?php $product=$article_each['product']; ?>
-               <?php $work=$article_each['work']; ?>
-               <?php $treatment1=$article_each['treatment1']; ?>
-               <?php $treatment2=$article_each['treatment2']; ?>
-               <?php $treatment3=$article_each['treatment3']; ?>
-               <?php $treatment4=$article_each['treatment4']; ?>
-               <?php $treatment5=$article_each['treatment5']; ?>
-               <?php $treatment6=$article_each['treatment6']; ?>
-               <?php $landscape=$article_each['landscapes']; ?>
-               <?php $comment=$article_each['comment']; ?>
-               <?php $favorite_flag=$article_each['favorite_flag']['favorite_count']; ?>
-      
+   
+  <div class="col-md-6">
 
-            <?php include('card.php'); ?>
+    <?php require('card.php'); ?>
+  </div>
 
-            <?php } ?>
-
-           </div>
-        </div>
-     </div>
+  <?php }} ?>
+  </div>
 
             <div class="row row-margin-bottom">
-
                <div class="align-center">
-                  <a href="top.php?=<?php echo $page +1; ?>" class="btn btn-default btn-load-destination"><span class="text">新着募集をもっと見る</span><i class="icon-spinner6"></i></a>
-                   <ul class="paging">
+               <ul class="paging">
            
                 <li>
                 <?php if ($page >1){ ?>
-
-              
-                <a href="index.php?page=<?php echo $page -1; ?>" class="btn btn-default">戻る</a>
+                <a href="top.php?page=<?php echo $page -1; ?>" class="btn btn-info btn-load-destination"><span class="text">戻る</span>
+                <i class="icon-spinner6"></i></a></li>
                 <?php } else { ?>
-                前
+              　　戻る
                 <?php } ?>
                 </li>
                 &nbsp;&nbsp;|&nbsp;&nbsp;
                 <li>
                 <?php if($page < $max_page){?>
 
-
-                <a href="index.php?page=<?php echo $page +1; ?>" class="btn btn-default btn-load-destination">新着情報をもっと見る</a></li>
+                <a href="top.php?page=<?php echo $page +1; ?>#new_article" class="btn btn-info btn-load-destination"><span class="text">新着募集をもっと見る</span>
+                <i class="icon-spinner6"></i></a></li>
                 <?php }else{ ?>
-                 次
+                 新着情報をもっと見る
                  <?php } ?>
-
                 </li>
-          </ul>
+         　　　 </ul>
                </div>
             </div>
 
@@ -356,15 +440,67 @@ while ($record = $stmt->fetch(PDO::FETCH_ASSOC)){
          <div class="row">
 
           <hr>
+    <?php if (isset($admin_article)) { ?>
+    <?php foreach ($admin_article as $admin_article_each) { ?>
+          <div class="col-md-6 col-sm-6 col-xs-12">
+
+    <?php $article_id=$admin_article_each['article_id']; ?>
+    <?php $title=$admin_article_each['title']; ?>
+    <?php $prefecture=$admin_article_each['prefecture']; ?>
+    <?php $place=$admin_article_each['place']; ?>
+    <?php $access=$admin_article_each['access']; ?>
+    <?php $start=$admin_article_each['start']; ?>
+    <?php $finish=$admin_article_each['finish']; ?>
+    <?php $product=$admin_article_each['product']; ?>
+    <?php $work=$admin_article_each['work']; ?>
+    <?php $treatment1=$admin_article_each['treatment1']; ?>
+    <?php $treatment2=$admin_article_each['treatment2']; ?>
+    <?php $treatment3=$admin_article_each['treatment3']; ?>
+    <?php $treatment4=$admin_article_each['treatment4']; ?>
+    <?php $treatment5=$admin_article_each['treatment5']; ?>
+    <?php $treatment6=$admin_article_each['treatment6']; ?>
+    <?php $landscape=$admin_article_each['landscapes']; ?>
+    <?php $comment=$admin_article_each['comment']; ?>
+    <!-- <?php // $favorite_flag=$admin_article_each['favorite_flag']['favorite_count']; ?> -->
+    <?php if(isset($_SESSION['apply_flag'])){
+      $apply_flag=$_SESSION['apply_flag'];}?>
+
+    <?php require('card.php'); ?>
+  </div>
+
+  <?php }} ?>
+          </div>
+       </div>
+            <div class="row row-margin-bottom">
+               <div class="align-center">
+               <ul class="paging">
+           
+                <li>
+                <?php if ($page >1){ ?>
+                <a href="top.php?page=<?php echo $page -1; ?>" class="btn btn-info btn-load-destination"><span class="text">戻る</span>
+                <i class="icon-spinner6"></i></a></li>
+                <?php } else { ?>
+              　　戻る
+                <?php } ?>
+                </li>
+                &nbsp;&nbsp;|&nbsp;&nbsp;
+                <li>
+                <?php if($page < $max_page){?>
+
+                <a href="top.php?page=<?php echo $page +1; ?>#new_article" class="btn btn-info btn-load-destination"><span class="text">おすすめ情報をもっと見る</span>
+                <i class="icon-spinner6"></i></a></li>
+                <?php }else{ ?>
+                 おすすめ情報をもっと見る
+                 <?php } ?>
+                </li>
+         　　　 </ul>
+               </div>
+            </div>
 
 
           </div>
        </div>
- 
 
-               <div class="align-center">
-                  <a href="#" class="btn btn-default btn-load-boats"><span class="text">おすすめをもっと見る</span><i class="icon-spinner6"></i></a>
-               </div>
             </div>
          </section>
       </div>
